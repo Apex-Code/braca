aa0cbd1525e2a263a925f6bef01affd089146e27
David Mars <upstaterounder@gmail.com>
2017-12-08 16:44:14 -0600

    update braca.js to switch class of clicked list items

--- assets/js/braca.js
+++ assets/js/braca.js
@@ -1,1 +1,8 @@
-// alert('Connected!!!');
+$("li").click(function(){
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        $(this).removeClass("finishedItem");
+    } else {
+        $(this).addClass("finishedItem");
+    }
+});
+
