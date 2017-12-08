8276981b479a5ec8dcc84a95d7443ae01a8c5f24
David Mars <upstaterounder@gmail.com>
2017-12-08 16:47:24 -0600

    refactor to toggleClass vice if/else

--- assets/js/braca.js
+++ assets/js/braca.js
@@ -1,8 +1,4 @@
 $("li").click(function(){
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).removeClass("finishedItem");
-    } else {
-        $(this).addClass("finishedItem");
-    }
+    $(this).toggleClass("finishedItem");
 });
 
