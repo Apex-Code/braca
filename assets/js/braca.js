f23709c87c8f294f370a359505c1e4d03cdc9631
David Mars <upstaterounder@gmail.com>
2017-12-08 20:43:22 -0600

    update logic to account for new Items being added to the list.

--- assets/js/braca.js
+++ assets/js/braca.js
@@ -1,4 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("finishedItem");
 });
 
+$("ul").on("click", "span", function(e) {
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13) {
+        var listItem = $(this).val();
+        $(this).val("");
+        $(".currentList").append("<li><span>X </span>"+listItem+"</li>");
+    }
+});
