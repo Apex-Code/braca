08a97a11a2c72d2eaa571959c104b921f5cfa74b
David Mars <upstaterounder@gmail.com>
2017-12-08 23:53:06 -0600

    added multiple styling and animated delete icon

--- assets/js/braca.js
+++ assets/js/braca.js
@@ -1,18 +1,22 @@
-$("ul").on("click", "li", function(){
+$("ul").on("click", "li", function () {
     $(this).toggleClass("finishedItem");
 });
 
-$("ul").on("click", "span", function(e) {
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     e.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(e){
-    if(e.which === 13) {
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
         var listItem = $(this).val();
         $(this).val("");
-        $(".currentList").append("<li><span>X </span>"+listItem+"</li>");
+        $(".currentList").append("<li><span><i class='fa fa-trash'></i></span>" + listItem + "</li>");
     }
+});
+
+$(".fa-pencil").click(function () {
+    $("input").fadeToggle();
 });
